2ce7dc4c36eccb94532b7413e278b770dd20670e
Tan Luong <tanluong12@gmail.com>
2020-05-12 14:27:11 -0700

    working qwer using two arrays

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import AudioPlayer from "../src/components/AudioPlayer";
 function App() {
+  const audioPlayer = AudioPlayer();
+  const notesArray = ["E4", "G4", "B4", "D5"];
+  const keyArray = ["q", "w", "e", "r"];
+
+  useEffect(() => {
+    audioPlayer.setInstrument("acoustic_grand_piano");
+  }, []);
+
+  const handleClick = () => {
+    audioPlayer.playNote("E4");
+  };
+  const handleClick1 = () => {
+    audioPlayer.playNote("G4");
+  };
+  const handleClick2 = (event) => {
+    let keyArrayIndex = keyArray.indexOf(event.key);
+    let keyMatch = notesArray[keyArrayIndex];
+    console.log(keyArrayIndex);
+    console.log(keyMatch);
+    audioPlayer.playNote(keyMatch);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <button onClick={handleClick}>Play</button>
+      <button onClick={handleClick1}>Play</button>
+      <button onKeyDown={handleClick2}>Play</button>
     </div>
   );
 }
